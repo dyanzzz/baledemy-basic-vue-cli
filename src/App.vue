7224c3bfc7d5952496c9f8daf6e0492dfab2aec1
Dian Tri Budianto <dyanzzz@gmail.com>
2021-07-25 21:59:07 +0700

    implement sample data & event handling

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png" width="200">
+      <h3>Mobile Legend : Bang bang</h3>
+      <button @click="changeHero">Change Hero</button>
+    </header>
+
+    <main>
+      <ul>
+        <li>
+          <img :src="image" alt="">
+          <p>{{ hero.name }}</p>
+          <p>Jenis Hero: <em>{{ hero.type }}</em></p>
+        </li>
+      </ul>
+    </main>
+
+    <footer>
+      <p>&copy; 2021 - Kelas Vue CLI</p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
+  data: function() {
+    return {
+      hero: {
+        name: 'Aurora',
+        type: 'Mage',
+        image: 'aurora.jpg'
+      }
+    }
+  },
+  methods: {
+    changeHero: function() {
+      this.hero = {
+        name: 'Zilong',
+        type: 'Figter',
+        image: 'zilong.jpg'
+      }
+    }
+  },
+  computed: {
+    image: function() {
+      return require('./assets/hero/' + this.hero.image)
+    }
+  },
 }
 </script>
 
@@ -21,8 +61,12 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+}
+main img {
+  width: 80px;
+  border: 1px solid #ccc;
 }
 </style>
